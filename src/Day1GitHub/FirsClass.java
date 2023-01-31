ef63271e14d80025b8a156c935f205754502fce4
Selimyagci45 <123612461+Selimyagci45@users.noreply.github.com>
2023-01-31 18:55:52 -0500

    Update FirsClass.java
    I added country

--- src/Day1GitHub/FirsClass.java
+++ src/Day1GitHub/FirsClass.java
@@ -2,6 +2,6 @@ package Day1GitHub;
 
 public class FirsClass {
     public static void main(String[] args) {
-        System.out.println("i am from turkey");
+        System.out.println("i am from Canada");
         System.out.println(1+1);
     }}
